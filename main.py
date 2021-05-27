18009e0840744aa094eb7855cb31fdd48ac9a197
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-05-27 20:34:18 +0530

    improve logging

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import functools
 import os
 import typing as t
+from pathlib import Path
+from sys import stderr
 
 import requests
 from PIL import Image
 from loguru import logger
 
+LOG_FILE = Path('all.log')
+
+sink_handler = {
+    'sink': stderr,
+    'level': 'INFO',
+    'backtrace': False,
+    'diagnose': False
+}
+base_log_file_handler = {
+    'sink': LOG_FILE,
+    'level': 'TRACE',
+    'rotation': '1 day',
+    'retention': 0
+}
+colorised_log_file_handler = base_log_file_handler | {
+    'sink': LOG_FILE.with_stem(f'colorised-{LOG_FILE.stem}'),
+    'colorize': True
+}
+
+handlers = [sink_handler, base_log_file_handler, colorised_log_file_handler]
+logger.configure(handlers=handlers)
+
 HEADERS = {'Authorization': f'Bearer {os.getenv("TOKEN")}'}
+API_URL = 'https://pixels.pythondiscord.com/{0}'.format
 
 URL = t.TypeVar('URL', str, bytes)
-
-logger.add('all.log', level='TRACE')
 
 
 def request(func: t.Callable[..., requests.Response]
@@ -35,8 +58,8 @@ def request(func: t.Callable[..., requests.Response]
 get = request(requests.get)
 post = request(requests.post)
 
-board = get("https://pixels.pythondiscord.com/get_pixels")
-size_json = get("https://pixels.pythondiscord.com/get_size").json()
+board = get(API_URL('get_pixels'))
+size_json = get(API_URL('get_size')).json()
 size = size_json['width'], size_json['height']
 
 img = Image.frombytes(
