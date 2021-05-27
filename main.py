fdf7d5c3a643142104aa16c2be33fd09f9ab2d3e
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-05-27 22:54:59 +0530

    properly working script

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import functools
+import itertools
 import os
+import random
+import time
 import typing as t
 from pathlib import Path
 from sys import stderr
@@ -38,14 +41,15 @@ def request(func: t.Callable[..., requests.Response]
             ) -> t.Callable[[str], requests.Response]:
     """Handle logging, url completion and rate-limits."""
     @functools.wraps(func)
-    def request(url: str) -> requests.Response:
+    def request(url: str, **kwargs: t.Any) -> requests.Response:
         logger.debug('Sending {0} request to {1}', func.__name__.upper(), url)
-        response = func(API_URL + url, headers=HEADERS)
+        response = func(API_URL + url, headers=HEADERS, **kwargs)
 
         try:
             response.raise_for_status()
         except requests.HTTPError:
             logger.exception('Exception when sending request:')
+            raise
 
         logger.trace('Recieved response headers={0} content={1}',
                      response.headers, response.content)
@@ -57,13 +61,56 @@ get = request(requests.get)
 post = request(requests.post)
 
 
-board = get('get_pixels')
-size_json = get('get_size').json()
-size = size_json['width'], size_json['height']
+def to_hex(rgb: tuple[str, str, str]) -> str:
+    """Return the hex representation of an rgb tuple."""
+    return ''.join(format(colour, '02x') for colour in rgb)
 
-img = Image.frombytes(
-    'RGB',
-    size,
-    board.content
-)
-img.show()
+
+class Pixel(t.NamedTuple):
+    """Represents a pixel to be changed."""
+
+    x: int
+    y: int
+    rgb: str
+
+
+drawing = Image.open('draw.png').convert('RGB')
+
+while True:
+    board_response = get('get_pixels')
+    size_json = get('get_size').json()
+    size = size_json['width'], size_json['height']
+    board = Image.frombytes(
+        'RGB',
+        size,
+        board_response.content
+    )
+    drawing_width, drawing_height = drawing.size
+    start_x, start_y = (int(os.environ[key]) for key in ('X', 'Y'))
+
+    differences = []
+    for x, y in itertools.product(range(drawing_width), range(drawing_height)):
+        drawing_pixel = drawing.getpixel((x, y))
+        corresponding_board_coord = start_x + x, start_y + y
+        img_pixel = board.getpixel(corresponding_board_coord)
+        if img_pixel != drawing_pixel:
+            change_pixel = Pixel(*corresponding_board_coord,
+                                 to_hex(drawing_pixel))
+            differences.append(change_pixel)
+
+    logger.debug('Remaining changes: {0}', differences)
+    logger.info('{0} changes remaining.', len(differences))
+    to_change = random.choice(differences)
+    logger.debug('Attempting to change {0}', to_change)
+
+    data = {'x': to_change.x, 'y': to_change.y, 'rgb': to_change.rgb}
+    post_response = post('set_pixel', json=data)
+    logger.info(post_response.json()['message'])
+
+    responses = [board_response, post_response]
+    remaining_reqs = [int(resp.headers['Requests-Remaining'])
+                      for resp in responses]
+    if not all(remaining_reqs):
+        resets = [int(resp.headers['Requests-Reset'])
+                  for resp in responses]
+        time.sleep(max(resets))
