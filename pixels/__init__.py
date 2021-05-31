94986413689fd34145da914ac8023dd380497cd5
Qwerty-133 <74311372+Qwerty-133@users.noreply.github.com>
2021-05-31 14:03:14 +0530

    retention is based on file size not date

--- pixels/__init__.py
+++ pixels/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 from sys import stdout
 
@@ -20,7 +21,7 @@ sink_handler = {
 base_log_file_handler = {
     'sink': LOG_FILE,
     'level': 'TRACE',
-    'rotation': '1 day',
+    'rotation': '20 MB',
     'retention': 0
 }
 colorised_log_file_handler = base_log_file_handler | {
@@ -30,3 +31,29 @@ colorised_log_file_handler = base_log_file_handler | {
 
 handlers = [sink_handler, base_log_file_handler, colorised_log_file_handler]
 logger.configure(handlers=handlers)
+
+
+class InterceptHandler(logging.Handler):
+    """Intercept logs from `logging` into `loguru`."""
+
+    def emit(self, record: logging.LogRecord) -> None:
+        """Emit the actual logrecords into `logger`."""
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage())
+
+
+root_logger = logging.getLogger('')
+root_logger.setLevel(logging.WARNING)
+root_logger.addHandler(InterceptHandler())
